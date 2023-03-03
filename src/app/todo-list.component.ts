65318718041db186fed2170e1461ea042a3b9e78
Emmanuel Lebaramo Kema <emmanuel.lebaramo@gmail.com>
2023-03-03 23:40:11 +0100

    Need to correct displayed content

--- src/app/todo-list.component.ts
+++ src/app/todo-list.component.ts
@@ -6,9 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ["todo-list.component.css"]
 })
 export class AppTodoList {
+  items: Array<object> = []
 
   removeParent(e: MouseEvent): void {
     const parent: HTMLElement|null = (e.target as HTMLElement).parentElement
     parent?.remove()
+  }
+
+  addListItem(e: MouseEvent|KeyboardEvent, input: HTMLInputElement, cta: HTMLDivElement):void {
+    const element: HTMLButtonElement|null = (e.target as HTMLButtonElement)
+    const value: string = input.value 
+
+    const date: Date = new Date
+    const datePiped: string = date.getDate() + "/" + date.getMonth() + 1 + "/" + date.getFullYear();
+    
+    this.items.push({text: value, date: datePiped})
+
+    input.value = ''
   }
 }
